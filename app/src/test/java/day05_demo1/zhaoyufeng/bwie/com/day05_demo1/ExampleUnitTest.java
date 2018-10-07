335110f9e8d65dc386fa42d3d826cb061e08faf0
MonsieurZhao <1261641750@qq.com>
2018-10-07 18:55:33 +0800

    add

---
+++ app/src/test/java/day05_demo1/zhaoyufeng/bwie/com/day05_demo1/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package day05_demo1.zhaoyufeng.bwie.com.day05_demo1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
